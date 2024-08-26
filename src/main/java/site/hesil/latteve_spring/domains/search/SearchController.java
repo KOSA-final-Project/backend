8da9e5158e1b100144276461a3df4f75b1c0d054
Jade9846 <jadej9864@gmail.com>
2024-08-26 16:42:01 +0900

    Feat: opensearch config 추가

---
+++ src/main/java/site/hesil/latteve_spring/domains/search/SearchController.java
@@ -0,0 +1,42 @@
+package site.hesil.latteve_spring.domains.search;
+
+import lombok.RequiredArgsConstructor;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch._types.HealthStatus;
+import org.opensearch.client.opensearch.cluster.HealthResponse;
+import org.opensearch.client.opensearch.cluster.OpenSearchClusterClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : site.hesil.latteve_spring.domains.search
+ * fileName       : SearchController
+ * author         : Heeseon
+ * date           : 2024-08-26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-26        Heeseon       최초 생성
+ */
+@RestController
+@RequestMapping("/api/search")
+@RequiredArgsConstructor
+public class SearchController {
+    private final OpenSearchClient openSearchClient;
+
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        try {
+            // 클러스터 헬스 체크 API 호출
+            OpenSearchClusterClient clusterClient = openSearchClient.cluster();
+            HealthResponse healthResponse = clusterClient.health();
+            HealthStatus status = healthResponse.status();
+            return "OpenSearch cluster health status: " + status.toString();
+        } catch (Exception e) {
+            return "Failed to connect to OpenSearch: " + e.getMessage();
+        }
+    }
+}
---
+++ src/main/java/site/hesil/latteve_spring/global/opensearch/config/OpenSearchConfig.java
@@ -0,0 +1,60 @@
+package site.hesil.latteve_spring.global.opensearch.config;
+
+import org.apache.http.auth.AuthScope;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * packageName    : site.hesil.latteve_spring.global.opensearch.config
+ * fileName       : OpenSearchConfig
+ * author         : Heeseon
+ * date           : 2024-08-26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-26        Heeseon       최초 생성
+ */
+@Configuration
+public class OpenSearchConfig {
+
+    @Value("${spring.opensearch.host}")
+    private String host;
+
+    @Value("${spring.opensearch.username}")
+    private String username;
+
+    @Value("${spring.opensearch.password}")
+    private String password;
+
+
+    @Bean
+    public OpenSearchClient openSearchConfig(){
+        // 사용자 이름과 비밀번호 설정
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password)
+        );
+
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200, "https"))
+                .setHttpClientConfigCallback(
+                    httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+                .build();
+
+        OpenSearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new OpenSearchClient(transport);
+    }
+
+
+
+}
