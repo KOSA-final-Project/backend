02ad38a40f58947bc109a18811f2d1919469d1df
Jo0Yo0n <dlwndbs10@gmail.com>
2024-09-01 17:58:11 +0900

    Add: ProjectApplyRequest record 추가

---
+++ src/main/java/site/hesil/latteve_spring/domains/project/dto/project/request/ProjectApplyRequest.java
@@ -0,0 +1,17 @@
+package site.hesil.latteve_spring.domains.project.dto.project.request;
+
+import lombok.Builder;
+
+/**
+ * packageName    : site.hesil.latteve_spring.domains.project.dto.project.request
+ * fileName       : ProjectApplyRequest
+ * author         : JooYoon
+ * date           : 2024-09-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-01        JooYoon       최초 생성
+ */
+@Builder
+public record ProjectApplyRequest(Long memberId, Long jobId) {}
