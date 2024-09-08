bd8aa5cccd48306a05bb9f429c04c72c4017e49b
Jo0Yo0n <dlwndbs10@gmail.com>
2024-09-08 23:44:23 +0900

    Add: Alarm 위한 DTO 생성
    - 프로젝트 지원 알람을 위한 ProjectApplicationAlarm record 생성
    - 프로젝트 승인/거절 알람을 위한 ProjectApprovalResultAlarm record 생성

---
+++ src/main/java/site/hesil/latteve_spring/domains/alarm/dto/ProjectApplicationAlarm.java
@@ -0,0 +1,19 @@
+package site.hesil.latteve_spring.domains.alarm.dto;
+
+import lombok.Builder;
+
+/**
+ * packageName    : site.hesil.latteve_spring.domains.alarm.dto
+ * fileName       : ProjectApplicationAlarm
+ * author         : JooYoon
+ * date           : 2024-09-08
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-08        JooYoon       최초 생성
+ */
+
+// 지원 알람: 프로젝트명, 멤버닉네임, 직무명, 프로젝트리더아이디
+@Builder
+public record ProjectApplicationAlarm(String projectName, String nickname, String jobName, Long projectLeaderId) {}
